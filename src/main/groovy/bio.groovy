435c381b2350a0ad92bff344155e8403826895ef
Hao Wu <echowuhao@gmail.com>
2017-04-26 17:42:16 -0700

    biojava

---
+++ src/main/groovy/bio.groovy
@@ -0,0 +1,13 @@
+import org.biojava.nbio.core.sequence.io.IUPACParser
+import org.biojava.nbio.core.sequence.DNASequence
+
+def p = IUPACParser.getInstance()
+
+p.getTables().each {
+    println it.getName()
+}
+
+def dna = new DNASequence("ATCG")
+
+println dna.getReverseComplement().getSequenceAsString()
+
